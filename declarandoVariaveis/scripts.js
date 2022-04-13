3ed794e2fe2b96992fd9817665204babb219cafc
André Luiz <andre.doctors@gmail.com>
2022-04-13 15:21:30 -0300

    exercicio declarando variaveis

---
+++ declarandoVariaveis/scripts.js
@@ -0,0 +1,11 @@
+let myName = "André";
+let myAge = 32;
+
+let myFavoriteFood = "";
+myFavoriteFood = "Lasanha";
+
+let city = "";
+let country = "";
+let car = "";
+let estate = "";
+let aprovado = "";
