58ed3dae6a80b6ffe3a8870f885849a1e3c58c74
André Luiz <andre.doctors@gmail.com>
2022-04-13 16:09:05 -0300

    tarefa usando typeof

---
+++ declarandoVariaveis/typeof.js
@@ -0,0 +1,13 @@
+let tipoString = "String";
+
+let tipoNumero = 32;
+
+let myAge = 32;
+
+let firstName = "André";
+let lastName = "Luiz";
+
+let time = "It\'s time";
+console.log(time);
+
+console.log(typeof tipoNumero);
